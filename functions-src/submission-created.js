f92ef2797037d214800d58c361335fd84929fc3e
Amee <ameecova@yahoo.com>
2020-09-11 10:51:59 -0700

    Create Twilio API call

--- functions-src/submission-created.js
+++ functions-src/submission-created.js
@@ -1,4 +1,29 @@
+// require('dotenv').config();
+
+const {
+  TWILIO_ACCOUNT_SID,
+  TWILIO_AUTH_TOKEN,
+  TWILIO_PHONE_NUMBER
+} = process.env;
+
+const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 
 exports.handler = function(event, context, callback) {
-console.log('Submission created!')
-};
+    return client.messages.create({
+      from: TWILIO_PHONE_NUMBER,
+      to: `${+1}${phoneNumber}`,
+      body: `Thank you for registering to vote by mail!`
+    })
+    .then(() => {
+      console.log('success!')
+      callback(null, {
+        statusCode: 200,
+        body: 'Created'
+      })
+    })
+    .catch(e => {
+      console.log(e);
+      callback(e);
+    });
+
+}
