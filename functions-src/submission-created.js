d98bdaa1f979bc5f557fca461574e9dea1469f2e
Amee <ameecova@yahoo.com>
2020-09-18 16:46:59 -0700

    Define logic for selfie submission

--- functions-src/submission-created.js
+++ functions-src/submission-created.js
@@ -11,21 +11,41 @@ const client = require('twilio')(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
 exports.handler = function(event, context, callback) {
     const data = JSON.parse(event.body).payload.data
     console.log(data)
-    return client.messages.create({
-      from: TWILIO_PHONE_NUMBER,
-      to: `+1${data.phoneNumber}`,
-      body: `Thank you for registering to vote by mail!`
-    })
-    .then(() => {
-      console.log('success!')
-      callback(null, {
-        statusCode: 200,
-        body: 'Created'
+    if (data.photo){
+      return client.messages.create({
+        from: TWILIO_PHONE_NUMBER,
+        to: `+1${data.phoneNumber}`,
+        body: `Thank you for registering to vote by mail! Nice selfie!`
       })
-    })
-    .catch(e => {
-      console.log(e);
-      callback(e);
-    });
+      .then(() => {
+        console.log('success!')
+        callback(null, {
+          statusCode: 200,
+          body: 'Created'
+        })
+      })
+      .catch(e => {
+        console.log(e);
+        callback(e);
+      });
+    } else{
+      return client.messages.create({
+        from: TWILIO_PHONE_NUMBER,
+        to: `+1${data.phoneNumber}`,
+        body: `Thank you for registering to vote by mail!`
+      })
+      .then(() => {
+        console.log('success!')
+        callback(null, {
+          statusCode: 200,
+          body: 'Created'
+        })
+      })
+      .catch(e => {
+        console.log(e);
+        callback(e);
+      });
+    }
+
 
 }
